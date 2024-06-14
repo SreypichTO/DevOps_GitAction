6382479132d5fe52d7304ade2d3ae714efd4db20
SreypichTO <sreypichdarani2003@gmail.com>
2024-06-14 14:33:10 +0700

    test success!

--- tests/Feature/CreateStudentTest.php
+++ tests/Feature/CreateStudentTest.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Tests\Feature;
-
+use function PHPUnit\Framework\assertTrue;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,27 +13,14 @@ class CreateStudentTest extends TestCase
     /** @test */
     public function it_creates_a_student()
     {
-        // $response = $this->post('/students', [
-        //     'name' => 'John Doe',
-        //     'email' => 'john.doe@itc.edu.kh',
-        //     'phone' => '012 345 678',
-        //     'dob' => '2003-10-01'
-        // ]);
-
-        // $response->assertStatus(201); // Assuming a 201 status code on successful creation
-
-        // $this->assertDatabaseHas('students', [
-        //     'name' => 'John Doe',
-        //     'email' => 'john.doe@itc.edu.kh',
-        //     'phone' => '012 345 678',
-        //     'dob' => '2003-10-01'
-        // ]);
         $student = Student::create([
             'name' => 'John Doe',
             'email' => '<your_name>@itc.edu.kh',
             'phone' => '012 345 678',
             'dob' => '2003-10-01'
         ]);
-        // assertTrue($student->exists());
+        assertTrue($student->exists());
+
+
     }
 }
